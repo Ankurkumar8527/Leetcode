0c2949e5ce2d948545e39e2713a57bf09b94c33b
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-06-24 18:18:58 +0530

    Time: 1 ms (73.56%), Space: 43.8 MB (14.76%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -18,18 +18,17 @@ class Solution {
         if(root==null) return 0;
         return 1+Math.max(levels(root.left),levels(root.right));
     }
-    public void postorder(TreeNode root,int level,List<Integer> ans){
+    public void DFS(TreeNode root,int level,List<Integer> ans){
         if(root==null) return;
-        postorder(root.right,level+1,ans);
-        if(ans.get(level)==-101)ans.set(level,root.val);
-        postorder(root.left,level+1,ans);
-        
+        DFS(root.left,level+1,ans);
+        ans.set(level,root.val);
+        DFS(root.right,level+1,ans);
     }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         int n = levels(root);
-        for(int i=0;i<n;i++) ans.add(-101);
-        postorder(root,0,ans);
+        for(int i=0;i<n;i++) ans.add(0);
+        DFS(root,0,ans);
         return ans;
     }
 }
