63d722bc60e3df0eea91131b665eaab65181bb24
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-06-24 16:44:53 +0530

    Time: 1 ms (13.16%), Space: 43 MB (84.49%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -15,20 +15,15 @@
  */
 class Solution {
     public List<Integer> DFS(TreeNode root){
-        // Using Two Stack
+        // Using Stack and concept of reversePreorder
         List<Integer> ans = new ArrayList<>();
         Stack<TreeNode> s1 = new Stack<>();
-        Stack<TreeNode> s2 = new Stack<>();
         if(root!=null) s1.push(root);
         while(!s1.isEmpty()){
             TreeNode temp = s1.pop();
-            s2.push(temp);
+            ans.add(0,temp.val);
             if(temp.left!=null) s1.push(temp.left);
             if(temp.right!=null) s1.push(temp.right);
-        }
-        while(!s2.isEmpty()) {
-            TreeNode t = s2.pop();
-            ans.add(t.val);
         }
         return ans;
     }
