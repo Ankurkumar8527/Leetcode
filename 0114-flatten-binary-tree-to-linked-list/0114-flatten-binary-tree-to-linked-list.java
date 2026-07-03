54ba76dfedfc56dedc81cc6582a49a68405fd103
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-03 17:40:38 +0530

    Time: 0 ms (100%), Space: 43.9 MB (52.11%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,27 +14,16 @@
  * }
  */
 class Solution {
-    public void Morris(TreeNode root,List<Integer> arr){
-        if(root==null) return;
-        arr.add(root.val);
-        Morris(root.left,arr);
-        Morris(root.right,arr);
-    }
     public void flatten(TreeNode root) {
         if(root==null) return;
-        List<Integer> arr = new ArrayList<>();
-        TreeNode curr=root;
-        Morris(curr,arr);
+        TreeNode lst = root.left;
+        TreeNode rst  = root.right;
+        flatten(lst);
+        flatten(rst);
         root.left=null;
-        root.right=null;
-        int k = 1;
+        root.right=lst;
         TreeNode temp = root;
-        for(int val : arr){
-            if(val==root.val && k>0) {
-                k--;
-                continue;}
-            temp.right=new TreeNode(val);
-            temp=temp.right;
-        }
+        while(temp.right!=null) temp=temp.right;
+        temp.right=rst;
     }
 }
