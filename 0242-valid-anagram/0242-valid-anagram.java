5b31dd0557d625d1ddcced85ada16095a5d87f3d
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-05 11:06:16 +0530

    Time: 5 ms (61.63%), Space: 44 MB (94.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        HashMap<Character,Integer> set1 = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            if(!set1.containsKey(ch)) set1.put(ch,1);
-            else set1.put(ch,set1.get(ch)+1);
+        int[] freq = new int[26];
+        // Arrays.fill(freq,0);
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
         }
-        HashMap<Character,Integer> set2 = new HashMap<>();
-        for(char ch : t.toCharArray()){
-            if(!set2.containsKey(ch)) set2.put(ch,1);
-            else set2.put(ch,set2.get(ch)+1);
-        }
-        if(set1.size()!=set2.size()) return false;
-
-        for(char key : set1.keySet()){
-            if(!set2.containsKey(key)) return false;
-            int f1 = set2.get(key);
-            int f2 = set1.get(key);
-            if(f1!=f2) return false;
-        }
-        for(char key : set2.keySet()){
-            if(!set1.containsKey(key)) return false;
-            int f1 = set2.get(key);
-            int f2 = set1.get(key);
-            if(f1!=f2) return false;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]!=0) return false;
         }
         return true;
     }
