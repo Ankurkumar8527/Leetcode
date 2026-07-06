08fc5225f76930897bbaef1a0e20f8916182e90d
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-06 15:01:47 +0530

    Time: 4 ms (100%), Space: 84.3 MB (83.03%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -1,22 +1,15 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int val:arr){
-            int key = ((val%k)+k)%k;
-            if(!map.containsKey(key)) map.put(key,1);
-            else  map.put(key,map.get(key)+1);
+        int n = arr.length;
+        int[] map = new int[k];
+        for(int i=0;i<n;i++){
+            int val = ((arr[i]%k)+k)%k;
+            map[val]++;
         }
-        int i = 0;
-        for(int key : map.keySet() ){
-            if(i>map.size()/2) break;
-            int val = k-key;
-            val = ((val%k)+k)%k;
-            if(!map.containsKey(val)) return false;
-            int f1 = map.get(val);
-            if(val==0 && f1%2!=0) return false;
-            int f2 = map.get(key);
-            if(f1!=f2) return false; 
-            i++;
+        if(map[0]%2!=0) return false;
+        for(int i=1;i<=k/2;i++){
+            int rem = k-i;
+            if(map[rem]!=map[i]) return false;
         }
         return true;
     }
