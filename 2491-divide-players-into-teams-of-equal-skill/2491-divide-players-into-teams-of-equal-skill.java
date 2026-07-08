a332380d4a2ddb8eaed6ca12c0dd20ca10440024
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-08 14:15:48 +0530

    Time: 27 ms (10.68%), Space: 76.9 MB (28.54%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -4,22 +4,12 @@ class Solution {
         int m = n/2;
         int[][] arr = new int[m][2];
         Arrays.sort(skill);
-        int i =0,j=n-1;
-        int k=0;
-        int prevSum=0;
-        long ans =0;
-        while(i<j){
-            arr[k][0]=skill[i];
-            arr[k][1]=skill[j];
-            if(k>0){
-                int val = arr[k][0]+arr[k][1];
-                if(val!=prevSum) return -1;
-            }
-            prevSum = arr[k][0]+arr[k][1];
-            ans += arr[k][0]*arr[k][1];
-            k++;
-            i++;
-            j--;
+        int prevSum=skill[0]+skill[n-1];
+        long ans = skill[0]*skill[n-1];
+        for(int i=1;i<m;i++){
+            int sum = skill[i]+skill[n-i-1];
+            if(sum!=prevSum) return -1;
+            ans+=skill[i]*skill[n-i-1];
         }
         return ans;
     }
