07efcfa69753bbfe67cd965b957fb25dbd27ff6f
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-11 18:17:48 +0530

    Time: 3 ms (98.08%), Space: 85.1 MB (11.36%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -3,12 +3,11 @@ class Solution {
         int[] ans = new int[n];
         for(int i=0;i<bookings.length;i++){
             int fidx = bookings[i][0]-1;
-            int sidx = bookings[i][1]-1;
-            for(int j=fidx;j<=sidx;j++)
-            {
-                ans[j] = ans[j]+bookings[i][2];
-            }
+            int sidx = bookings[i][1];
+            ans[fidx]+=bookings[i][2];
+            if(sidx<n) ans[sidx]=ans[sidx]-bookings[i][2];
         }
+        for(int i=1;i<n;i++) ans[i]+=ans[i-1];
         return ans;
     }
 }
