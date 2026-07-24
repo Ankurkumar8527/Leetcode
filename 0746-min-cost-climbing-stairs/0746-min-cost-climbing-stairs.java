1837891c4d4c61151920339202e72c41c402626c
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-24 12:12:51 +0530

    Time: 1 ms (39.54%), Space: 44.8 MB (58.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,15 +1,13 @@
 class Solution {
-    public int minCost(int i ,int[] cost,int[] dp){
-        if(i<=1) return cost[i];
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]=cost[i]+Math.min(minCost(i-1,cost,dp),minCost(i-2,cost,dp));
-    }
     public int minCostClimbingStairs(int[] cost) {
-        int n =  cost.length;
-        // No of distict calls =  n 
-        // Therefore, dp size is n-1 to 0 
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return Math.min(minCost(n-1,cost,dp),minCost(n-2,cost,dp));
+        // Tabulation Method 
+        int n = cost.length;
+        int[] dp =  new int[n];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
