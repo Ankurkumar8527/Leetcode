d3bd92016227995eb982df35c1b7e6fc169df66e
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-24 12:43:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.68%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,14 +1,11 @@
 class Solution {
-    public int tribo(int n,int[] dp) {
-        //  Memoization Method
-        if(n==0 || n==1) return n;
-        if(n==2) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=tribo(n-1,dp)+tribo(n-2,dp)+tribo(n-3,dp);
-    }
     public int tribonacci(int n) {
+        // Tabulation 
+        if(n==0 || n==1) return n;
         int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return tribo(n,dp);
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++) dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        return dp[n];
     }
 }
