0818bf877483ca498b206679a8abcc536753f228
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-25 15:39:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int noofpaths(int m, int n,int[][] dp ){
-        if(n==1 || m==1) return 1;
-        if(dp[m][n]!=0) return dp[m][n];
-        int r = noofpaths(m-1,n,dp)%2000000000;
-        int d =  noofpaths(m,n-1,dp)%2000000000;
-        return dp[m][n]=(r+d)%2000000000;
-    }
     public int uniquePaths(int m, int n) {
-        // Memoization
-        int[][] dp = new int[m+1][n+1];
-        return noofpaths(m,n,dp);
+        // Tabulation Method
+        if(m==1 && n==1) return 0;
+        if(m==1 || n==1) return Math.min(m,n);
+        int[][] dp = new int[m][n];
+        for(int i=0;i<n;i++) dp[0][i]=1;
+        for(int i=0;i<m;i++) dp[i][0]=1;
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
