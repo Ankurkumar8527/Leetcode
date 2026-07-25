12e12b80bbd82d80bd69ae6958971a2337da076d
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-25 15:07:51 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.19%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int noofpaths(int m, int n,int[][] dp ){
+        if(n==1 || m==1) return 1;
+        if(dp[m][n]!=0) return dp[m][n];
+        int r = noofpaths(m-1,n,dp)%2000000000;
+        int d =  noofpaths(m,n-1,dp)%2000000000;
+        return dp[m][n]=(r+d)%2000000000;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        // Arrays.fill(dp,-1);
+        return noofpaths(m,n,dp);
+    }
+}
