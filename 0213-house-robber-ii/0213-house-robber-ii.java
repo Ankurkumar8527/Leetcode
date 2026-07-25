6d035b847ee7b9c3ab892cc776fef28f2e0a458e
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-25 12:04:51 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,21 +1,20 @@
 class Solution {
-
-     public int amount(int[] nums,int i, int[] dp,int end){
-        if(i>=end) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take = nums[i]+amount(nums,i+2,dp,end);
-        int skip = amount(nums,i+1,dp,end);
-        return dp[i]=Math.max(take,skip);
-    }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        int[] dp1 = new int[n];
+        if(n==2) return Math.max(nums[0],nums[1]);
+        int[] dp1 = new int[n-1];
+        dp1[0]=nums[0];
+        dp1[1]=Math.max(nums[1],nums[0]);
+        for(int i=2;i<n-1;i++){
+            dp1[i]=Math.max(dp1[i-1],nums[i]+dp1[i-2]);
+        }
         int[] dp2 = new int[n];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
-        return Math.max(
-            amount(nums,0,dp1,n-1),
-            amount(nums,1,dp2,n));
+        dp2[1]=nums[1];
+        dp2[2]=Math.max(nums[1],nums[2]);
+        for(int i=3;i<n;i++){
+            dp2[i]=Math.max(dp2[i-1],nums[i]+dp2[i-2]);
+        }
+        return Math.max(dp1[n-2],dp2[n-1]);
     }
 }
