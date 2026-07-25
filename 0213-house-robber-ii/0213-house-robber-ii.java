2d282519362b840405bc14ae5dffbd6424190e6c
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-25 11:43:01 +0530

    Time: 0 ms (100%), Space: 42.8 MB (61.25%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,21 @@
 class Solution {
 
-    public int amount1(int[] nums,int i, int[] dp1){
-        if(i>=nums.length-1) return 0;
-        if(dp1[i]!=-1) return dp1[i];
-        int take = nums[i]+amount1(nums,i+2,dp1);
-        int skip = amount1(nums,i+1,dp1);
-        return dp1[i]=Math.max(take,skip);
-    }
-     public int amount(int[] nums,int i, int[] dp){
-        if(i>=nums.length) return 0;
+     public int amount(int[] nums,int i, int[] dp,int end){
+        if(i>=end) return 0;
         if(dp[i]!=-1) return dp[i];
-        int take = nums[i]+amount(nums,i+2,dp);
-        int skip = amount(nums,i+1,dp);
+        int take = nums[i]+amount(nums,i+2,dp,end);
+        int skip = amount(nums,i+1,dp,end);
         return dp[i]=Math.max(take,skip);
     }
     public int rob(int[] nums) {
-         int n = nums.length;
-         if(n==1) return nums[0];
-        int[] dp1 = new int[n-1];
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
         Arrays.fill(dp1,-1);
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return Math.max(amount1(nums,0,dp1),amount(nums,1,dp));
+        Arrays.fill(dp2,-1);
+        return Math.max(
+            amount(nums,0,dp1,n-1),
+            amount(nums,1,dp2,n));
     }
 }
