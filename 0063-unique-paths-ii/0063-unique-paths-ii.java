3df343870dc8651a2259536bb06fcd91529d201e
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-27 16:49:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (64.92%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int uniquePath(int[][] nums,int[][] dp,int i,int j,int m,int n){
-        if(i>m || j>n) return 0;
-        if(nums[i][j]==1) return 0;
-        if(i==m && j==n) return 1;
-        if(dp[i][j]!=0) return dp[i][j];
-        int r = uniquePath(nums,dp,i,j+1,m,n);
-        int d = uniquePath(nums,dp,i+1,j,m,n);
-        return dp[i][j]=r+d;
-    }
     public int uniquePathsWithObstacles(int[][] nums) {
         int m = nums.length;
         int n = nums[0].length;
         int[][] dp = new int[m][n];
-        return uniquePath(nums,dp,0,0,m-1,n-1);
+        dp[0][0] = nums[0][0] == 1 ?0:1;
+        
+        for(int i=1;i<m;i++){
+            if(nums[i][0]==0) dp[i][0]=dp[i-1][0];
+        }  
+         for(int i=1;i<n;i++){
+            if(nums[0][i]==0) dp[0][i]=dp[0][i-1];
+        }  
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(nums[i][j]==0) dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
