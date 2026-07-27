7e8cf6516bc96dc7888fa42f236d775069c65282
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-27 15:37:50 +0530

    Time: 2 ms (78.92%), Space: 50.2 MB (56.78%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,20 +1,21 @@
 class Solution {
+    public int MinPath(int[][] grid,int[][] dp,int i,int j, int m, int n ){
+        if(i==m-1 && j==n-1) return grid[m-1][n-1];
+        if(i>=m || j>=n ) return Integer.MAX_VALUE;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int r = MinPath(grid,dp,i+1,j,m,n);
+        int d = MinPath(grid,dp,i,j+1,m,n);
+        return dp[i][j]=grid[i][j]+Math.min(r,d);
+    }
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-
-        dp[0][0] = grid[0][0];
-
-        for(int i=1;i<n;i++) dp[0][i]=dp[0][i-1]+grid[0][i];
-
-        for(int j=1;j<m;j++) dp[j][0]=dp[j-1][0]+grid[j][0];
-
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
             }
         }
-        return dp[m-1][n-1];
+        return MinPath(grid,dp,0,0,m,n);
     }
 }
