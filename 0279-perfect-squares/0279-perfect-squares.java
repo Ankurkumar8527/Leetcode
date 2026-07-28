7e45dc98698e9c12548398e661edace914c7e267
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-28 12:33:30 +0530

    Time: 196 ms (9.45%), Space: 44.9 MB (26.29%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean perfectSq(int n){
+        int sq = (int) Math.sqrt(n);
+        return (sq*sq==n);
+    }
+    public int minNum(int n,int[] dp){
+        if(perfectSq(n)) return 1;
+        int count = n;
+        if(dp[n]!=-1) return dp[n];
+        for(int i=1;i*i<=n;i++){
+            int val = minNum(i*i,dp)+minNum(n-i*i,dp);
+            count = Math.min(count,val);
+            dp[i]=count;
+        }
+        return dp[n]=count;
+    }
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return minNum(n,dp);
+    }
+}
