ef5a89e3e43a4dfb54b7677f399d33210ea2a6e2
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-28 13:55:44 +0530

    Time: 1 ms (94.75%), Space: 46.7 MB (13.73%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.java
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
         int n = nums.length;
+        if(n==1) return true;
         int mid = nums[n/2];
         int count=0;
         for(int i=0;i<n;i++) {
