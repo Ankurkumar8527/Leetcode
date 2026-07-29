56bfed0a227cbbae18c27905f97047a5c7260b5b
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-29 16:46:21 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,16 +2,23 @@ class Solution {
     public int uniquePaths(int m, int n) {
         if(n==1 || m==1) return 1;
         int[][] dp = new int[2][n];
-        dp[0][0] = 1; dp[1][0]=1;
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             dp[0][i]=1;
+            dp[1][i]=1;
         }
+
         for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[1][j]=dp[1][j-1]+dp[0][j];
-                dp[0][j]=dp[1][j];
+            if(i%2!=0){
+                for(int j=1;j<n;j++){
+                    dp[1][j]=dp[1][j-1]+dp[0][j];
+                }
+            }
+            else {
+                for(int j=1;j<n;j++){
+                    dp[0][j]=dp[0][j-1]+dp[1][j];
+                }
             }
         }
-        return dp[1][n-1];
+        return Math.max(dp[1][n-1],dp[0][n-1]);
     }
 }
