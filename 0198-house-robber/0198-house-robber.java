29624aa45110aaed9c01a2b06e50e25626bbc507
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-30 13:23:51 +0530

    Time: 0 ms (100%), Space: 42.6 MB (88.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
+        // Space Optimization
         int n = nums.length;
         if(n==1) return nums[0];
-        int[] dp = new int[n];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
+        int a = nums[0];
+        int b = Math.max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+            int  c =Math.max(nums[i]+a,b);
+            a = b;
+            b=c;
         }
-        return dp[n-1];
+        return b;
     }
 } 
