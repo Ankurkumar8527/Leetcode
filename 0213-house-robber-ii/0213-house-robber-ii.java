ff919bf533aa3bb2d9fcacc27f3e633d5b1d075d
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-30 13:57:18 +0530

    Time: 0 ms (100%), Space: 43 MB (8.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,25 +1,26 @@
 class Solution {
     public int rob(int[] nums) {
+        // Space Optimization
         int n = nums.length;
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
-        int[] dp = new int[n];
-
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
+        int a = nums[0];
+        int b = Math.max(nums[0],nums[1]);
         for(int i=2;i<=n-2;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+            int c = Math.max(nums[i]+a,b);
+            a=b;
+            b=c;
         }
-        int x = dp[n-2];
+        int x = b;
 
-        Arrays.fill(dp,0);
-
-        dp[1]=nums[1];
-        dp[2]=Math.max(nums[2],nums[1]);
+        a=nums[1];
+        b=Math.max(nums[2],nums[1]);
         for(int i=3;i<=n-1;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+            int c = Math.max(nums[i]+a,b);
+            a=b;
+            b=c;
         }
-        int y = dp[n-1];
+        int y = b;
         return Math.max(x,y);
     }
 }
