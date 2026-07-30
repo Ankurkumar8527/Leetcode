b7aa9ee5954806c77fe1efbd5052fdf03f722b09
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-07-30 12:28:09 +0530

    Time: 1 ms (39.26%), Space: 44.8 MB (76.17%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
+    public int MinCost(int n,int[] cost, int[] dp){
+        if(n==0 || n==1) return cost[n];
+        if(dp[n]!=-1) return dp[n];
+        int take = MinCost(n-1,cost,dp);
+        int skip =  MinCost(n-2,cost,dp);
+        return dp[n]=cost[n]+Math.min(take,skip);
+    }
     public int minCostClimbingStairs(int[] cost) {
-        // Tabulation Method 
         int n = cost.length;
-        int[] dp =  new int[n];
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2;i<n;i++){
-            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
-        }
-        return Math.min(dp[n-1],dp[n-2]);
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return Math.min(MinCost(n-1,cost,dp),MinCost(n-2,cost,dp));
     }
 }
