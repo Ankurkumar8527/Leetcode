d9db608873a9fadda846e6ba68865a3353afc1d4
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-05 17:14:51 +0530

    Time: 2 ms (33.8%), Space: 45.4 MB (63.17%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,20 @@
 class Solution {
-    public void permutations(int i,int n,int[] nums,List<Integer> path,List<List<Integer>> ans){
-        if(path.size()==n){
+    public void helper(int idx,int[] nums,List<Integer> path, List<List<Integer>> ans){
+        if(path.size()==nums.length){
             ans.add(new ArrayList<>(path));
             return;
         }
-
-        for(int j=0;j<n;j++){
-            if(!path.contains(nums[j])){
-                path.add(nums[j]);
-                permutations(j+1,n,nums,path,ans);
+        for(int i=0;i<nums.length;i++){
+            if(!path.contains(nums[i])){
+                path.add(nums[i]);
+                helper(i+1,nums,path,ans);
                 path.remove(path.size()-1);
             }
-    
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        permutations(0,n,nums,new ArrayList<>(),ans);
+        helper(0,nums,new ArrayList<>(),ans);
         return ans;
     }
 }
