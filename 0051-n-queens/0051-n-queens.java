de26f5db3241cb1945b26de9e4dcd6d643a9a4a5
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-06 15:34:58 +0530

    Time: 15 ms (7.77%), Space: 46.5 MB (82.76%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    public boolean isSafe(int row,int col,int n,List<List<String>> board){
+        int demorow=row;
+        int democol=col;
+
+        while(row>=0 && col>=0){
+            if(board.get(row).get(col).equals("Q")) return false;
+            row--;
+            col--;
+        }
+        row=demorow;
+        col=democol;
+        while(col>=0){
+            if(board.get(row).get(col).equals("Q")) return false;
+            col--;
+        }
+        row=demorow;
+        col=democol;
+        while(row<n && col>=0){
+            if(board.get(row).get(col).equals("Q")) return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+    public void placingQueens(int col,int n,List<List<String>> board,List<List<String>> ans){
+        if (col == n) {
+            List<String> temp = new ArrayList<>();
+
+            for (List<String> row : board) {
+                temp.add(String.join("", row));
+            }
+
+            ans.add(temp);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,n,board)){
+                board.get(row).set(col, "Q");
+                placingQueens(col+1,n,board,ans);
+                board.get(row).set(col, ".");
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+
+        List<List<String>> board = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            List<String> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(".");
+            }
+            board.add(row);
+        }
+        placingQueens(0,n,board,ans);
+        return ans;
+    }
+}
