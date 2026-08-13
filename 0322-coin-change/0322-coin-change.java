47f2043af196b6ef5f57df4189baadfd4fdf8e38
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-13 18:54:13 +0530

    Time: 25 ms (32.08%), Space: 47.4 MB (11.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,20 @@
 class Solution {
-    public long CountCoins(int i,int amount,int[] coins,long[][] dp){
-        if(i==coins.length){
-            if(amount==0) return 0;
-            else return Integer.MAX_VALUE;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        long skip = CountCoins(i+1,amount,coins,dp);
-        if(amount-coins[i]<0) return dp[i][amount]=skip;
-        long pick = 1+CountCoins(i,amount-coins[i],coins,dp);
-        return dp[i][amount] = Math.min(pick,skip);
-    }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
         long[][] dp = new long[n][amount+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=amount;j++) dp[i][j]=-1;
+
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                long skip = (i>0) ? dp[i-1][j] : ( j==0 ? 0 : Integer.MAX_VALUE);
+                if(j-coins[i]<0) dp[i][j]=skip;
+                else{
+                     long pick = 1 + dp[i][j-coins[i]];
+                     dp[i][j] = Math.min(pick,skip);
+                } 
+            }
         }
-        int ans =  (int)CountCoins(0,amount,coins,dp);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        int ans = (int)dp[n-1][amount];
+        return (ans==Integer.MAX_VALUE) ? -1 : ans;
     }
 }
