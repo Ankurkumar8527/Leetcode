1137ad22e26cee6c214bf7f8abf7ece343127484
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-13 16:50:15 +0530

    Time: 117 ms (7.09%), Space: 123.5 MB (5.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isSubset(int i,int arr[], int sum,int[][] dp){
-        if(i==arr.length){
+        if(i==-1){
             if(sum==0) return true;
             else return false;
         }
         if(dp[i][sum]!=-1) return (dp[i][sum]==1);
         boolean ans = false;
-        boolean skip = isSubset(i+1,arr,sum,dp);
+        boolean skip = isSubset(i-1,arr,sum,dp);
         if(sum-arr[i]<0) ans = skip;
         else{
-            boolean take = isSubset(i+1,arr,sum-arr[i],dp);
+            boolean take = isSubset(i-1,arr,sum-arr[i],dp);
             ans = take || skip;;
         } 
         if(ans) dp[i][sum]=1;
@@ -27,6 +27,6 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<=sum;j++) dp[i][j]=-1;
         }
-        return isSubset(0,nums,sum/2,dp);
+        return isSubset(n-1,nums,sum/2,dp);
     }
 }
