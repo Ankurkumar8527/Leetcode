c710bdfd26f7bb77dcf4624842d533dfb1acdc7d
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-16 20:18:47 +0530

    Time: 22 ms (45.73%), Space: 54.2 MB (20.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,11 +5,8 @@ class Solution {
         int[][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++) {
-                int p = (i>0 && j>0) ?  dp[i-1][j-1] : 0;
-                int q = (i>0) ? dp[i-1][j] : 0;
-                int r = (j>0) ? dp[i][j-1] : 0;
-                if(a.charAt(i)==b.charAt(j)) dp[i][j] = 1+ p;
-                else  dp[i][j] = Math.max(q,r);
+                if(a.charAt(i)==b.charAt(j)) dp[i][j] = 1+((i>0 && j>0) ? dp[i-1][j-1] : 0);
+                else  dp[i][j] = Math.max(((i>0) ? dp[i-1][j] : 0),((j>0) ? dp[i][j-1] : 0));
             }
         }
         return dp[m-1][n-1];
