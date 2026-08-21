de8f7db449dfc42c11d745b71f755fcf8c3b614c
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-21 11:24:02 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,33 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int i=0,j=n-1;
+        int a_score = 0;
+        int b_score = 0;
+        int k=0;
+        while(i<j){
+            if(k%2==0){
+                if(piles[i]>=piles[j]){
+                    a_score = piles[i];
+                    i++;
+                }
+                else{
+                    a_score = piles[j];
+                    j--;
+                }
+            }
+            else{
+                     if(piles[i]>=piles[j]){
+                    b_score = piles[i];
+                    i++;
+                }
+                else{
+                    b_score = piles[j];
+                    j--;
+                }
+            }
+            // k++;
+        }
+        return a_score>=b_score;
     }
 }
