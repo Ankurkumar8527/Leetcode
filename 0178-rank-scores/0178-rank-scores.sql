3a97c5dcd6bd592eb54585ec8bcbef98a63982cc
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-23 17:17:09 +0530

    Time: 373 ms (36.5%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT score , 
-       DENSE_RANK() OVER(ORDER BY score DESC) AS  `rank`
-FROM Scores;
+select score,
+dense_rank() over (order by score desc) as 'rank'
+from Scores
