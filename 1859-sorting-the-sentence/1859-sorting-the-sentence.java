c1e7c9c22b821666b1176307394a7eeb0d01b172
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-08-23 13:59:16 +0530

    Time: 2 ms (14.7%), Space: 43 MB (27.51%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -10,16 +10,16 @@ class Solution {
         String[] sb = new String[size];
         int i=0;
         while(i<s.length()){
-            if(s.charAt(i)==' '){
-                i++;
-                continue;
-            }
+            // if(s.charAt(i)==' '){
+            //     i++;
+            //     continue;
+            // }
             int j = i;
             while(j<s.length() && !Character.isDigit(s.charAt(j))) j++; 
             String substr = s.substring(i,j);
             int idx = s.charAt(j)-'0'-1;
             sb[idx]=substr;
-            i=j+1;
+            i=j+2;
         }
         String ans = sb[0];
         for(int j=1;j<size;j++) ans=ans+" "+sb[j];
