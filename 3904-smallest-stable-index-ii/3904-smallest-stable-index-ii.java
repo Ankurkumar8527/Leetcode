842d00febaf05de3488921c0a0c1babbb0a5c25a
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-09-05 22:56:55 +0530

    Time: 3 ms (100%), Space: 133.2 MB (20.99%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -4,16 +4,14 @@ class Solution {
         if(n==1 && nums[0]==k) return 0;
         int max = 0;
         int min = Integer.MAX_VALUE;
-        int[] rsum = new int[n];
         int[] lsum = new int[n];
-        for(int i=0;i<n;i++){
-            max = Math.max(max,nums[i]);
-            rsum[i]=max;
-            min = Math.min(min,nums[n-1-i]);
-            lsum[n-1-i]=min;
+        for(int i=n-1;i>=0;i--){
+            min = Math.min(min,nums[i]);
+            lsum[i]=min;
         }
         for(int i=0;i<n;i++){
-            if(rsum[i]-lsum[i]<=k) return i;
+             max = Math.max(max,nums[i]);
+            if(max-lsum[i]<=k) return i;
         }
         return -1;
     }
