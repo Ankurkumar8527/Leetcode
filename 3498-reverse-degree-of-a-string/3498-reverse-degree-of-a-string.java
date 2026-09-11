e4be0e7fffa844e3155bf81a71f2b7a7aa031a71
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-09-11 13:48:56 +0530

    Time: 1 ms (100%), Space: 44 MB (50.16%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int n = s.length();
+        int sum  = 0;
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            int value = 'z'-ch+1;
+            sum = sum +(i+1)*value; 
+        }
+        return sum;
+    }
+}
