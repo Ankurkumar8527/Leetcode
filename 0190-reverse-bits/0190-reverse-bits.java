3b0504d9ca7589e5ce3a5229bd6beab5033546e5
Ankurkumar8527 <148180313+Ankurkumar8527@users.noreply.github.com>
2026-09-22 17:21:20 +0530

    Time: 1 ms (59.52%), Space: 42.2 MB (92.34%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,21 +1,16 @@
 class Solution {
     public int reverseBits(int n) {
-        String bits = "";
-        int x = n;
-        while(x>0){
-            int d = x%2;
-            bits=d+bits;
-            x/=2;
+        int i = 0, j = 31;
+        while(i<j){
+            int iMask = (1<<i), jMask = (1<<j);
+            boolean iOn = (n&iMask)==0 , jOn = (n&jMask)==0;
+            if(iOn != jOn){
+                n^=iMask;
+                n^=jMask;
+            }
+            i++;
+            j--;
         }
-        int len = 32-bits.length();
-        while(len-->0)  bits="0"+bits;
-        int ans = 0, p = 1;
-        for(int i=0;i<32;i++){
-            int ele = bits.charAt(i)-'0';
-            ele = ele*p;
-            p*=2;
-            ans+=ele;
-        }
-        return ans;
+        return n;
     }
 }
